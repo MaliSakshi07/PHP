46282ddd66057c421e9505dc8273d48963dc751e
MaliSakshi07 <sakshimali467@gmail.com>
2025-06-26 22:17:43 +0530

    Basics Program

---
+++ Basic Programs/02 If_else_statement.php
@@ -0,0 +1,16 @@
+<?php
+    $No1 = 20;
+    $No2 = 10;
+
+    echo "No1 :", $No1, "<br>";
+    echo "No2 :", $No2, "<br>";
+
+    if($No1 > $No2)
+    {
+        echo $No1, " is Greater than ", $No2;
+    }
+    else
+    {
+        echo $No1, " is Less than ", $No2;
+    }
+?>
