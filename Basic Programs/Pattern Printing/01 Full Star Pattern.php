d6a8ab26541931a35c2c596f016ea58600834400
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-06 22:09:06 +0530

    Pattern Program

---
+++ Basic Programs/Pattern Printing/01 Full Star Pattern.php
@@ -0,0 +1,18 @@
+<?php
+    $i = 0;
+    $j = 0;
+    $x = 5;
+
+    echo "Full Star Pattern Program :<br><br>";
+
+    for($i = 1; $i <= $x; $i++)
+    {
+        for($j = 1; $j <= $x; $j++)
+        {
+            echo" * ";
+        }
+        echo"<br>";
+    }
+
+    echo "====================================";
+?>
